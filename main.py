ad9ab59c46a8be6d54fb3124c21c90fffa2c0331
Krzysztof Szumny <krzysztof.szumny@spistresci.pl>
2015-08-08 19:25:51 +0200

    wip wip

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import urllib2
 from lxml import etree
 from time import sleep
 from github import Github
+from termcolor import colored
 
 
 github = Github()
 
 def query_builder(requirenments=None, repos=None):
-    q = 'filename:requirenments.txt'
+    q = 'filename:requirements.txt'
     for entry in requirenments or []:
         package, version = entry.split('==')
         q += ' "{}"'.format(package)
@@ -22,7 +23,7 @@ def query_builder(requirenments=None, repos=None):
 
 requirenments = [
     "django==1.8",
-    "django-rest-framework==3.1.1",
+    "djangorestframework==3.1.1",
 ]
 
 query = query_builder(requirenments)
@@ -35,6 +36,10 @@ params = {
     'q': query,
 }
 
+xpath_selector1 = '//*[@id="code_search_results"]/div[1]/div/p/a[1]'
+# filename_xpath = '//*[@id="code_search_results"]/div[1]/div/p/a[2]/em'
+filename_url_xpath = '//*[@id="code_search_results"]/div[1]/div/p/a[2]/@href'
+
 
 repositories = []
 
@@ -45,40 +50,76 @@ while True:
     opener = urllib2.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
-    print ">>  PAGE: %d <<" % page
+    # print ">>  PAGE: %d <<" % page
     try:
         response = opener.open(url)
-    except urllib2.HTTPError, e:
+    except Exception as e:
         print str(e)
         sleep(70)
         response = opener.open(url)
 
     tree = etree.parse(response, etree.HTMLParser())
-    xpath_selector1 = '//*[@id="code_search_results"]/div[1]/div/p/a[1]'
-    elements = tree.xpath(xpath_selector1)
 
+    elements = tree.xpath(xpath_selector1)
     if not elements:
         break
 
-    repos = [elem.text for elem in elements]
+    filename_blob_urls = tree.xpath(filename_url_xpath)
 
-    repositories += repos
+    for filename_blob_url in filename_blob_urls:
+        filename_path = '/'.join(filename_blob_url.split('/')[5:])
 
-    api_query = query_builder(requirenments, repos)
-    results = github.search_code(api_query)
-    for result in results:
-        requirenments_path = result.html_url
-        raw_requirenments = requirenments_path.replace('https://github.com/', 'https://raw.githubusercontent.com/').replace('/blob/', '/')
+        if 'requirements' in filename_path:
+            requirements_raw_file = 'https://raw.githubusercontent.com' + filename_blob_url.replace('/blob/', '/')
+            response = opener.open(requirements_raw_file)
+            try:
+                file_content = response.read() + u'\n'
+            except:
+                print "problem" + response.read()
 
-        print raw_requirenments
-        response = opener.open(raw_requirenments)
-        print response.read()
+            status = "OK"
+            for entry in requirenments:
+                package, version = entry.split('==')
+
+                if entry in file_content:
+                    continue
+                else:
+                    status = "ALMOST"
+                    if package + u'\n' in file_content:
+                        continue
+                    else:
+                        status = "NO"
+                        break
+
+            if status != "NO":
+                print "Repository: " + 'http://github.com/' + '/'.join(filename_blob_url.split('/')[1:3])
+                print "Status: " + colored(status, 'green' if status == 'OK' else 'yellow')
+                print "File: " + filename_path
+
+                for line in file_content.split('\n'):
+                    if line:
+                        print colored('\t' + line, 'cyan')
+
+                print ""
+
+
+
+    # repos = [elem.text for elem in elements]
+    #
+    # repositories += repos
+    #
+    # api_query = query_builder(requirenments, repos)
+    # results = github.search_code(api_query)
+    # for result in results:
+    #     requirenments_path = result.html_url
+    #     raw_requirenments = requirenments_path.replace('https://github.com/', 'https://raw.githubusercontent.com/').replace('/blob/', '/')
+    #
+    #     print raw_requirenments
+    #     response = opener.open(raw_requirenments)
+    #     print response.read()
 
 
     page += 1
-
-
-
 
 
 # from lxml.etree import tostring
@@ -92,10 +133,10 @@ while True:
 # print str(results)
 #
 #
-for result in results:
-    requirenments_path = result.html_url
-    raw_requirenments = requirenments_path.replace('https://github.com/', 'https://raw.githubusercontent.com/').replace('/blob/', '/')
-
-    print raw_requirenments
+# for result in results:
+#     requirenments_path = result.html_url
+#     raw_requirenments = requirenments_path.replace('https://github.com/', 'https://raw.githubusercontent.com/').replace('/blob/', '/')
+#
+#     print raw_requirenments
 #
 # print "by"
