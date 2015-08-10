4a99202758272ead258a3f9cb18cd1da2f4b99f0
Krzysztof Szumny <krzysztof.szumny@spistresci.pl>
2015-08-10 21:57:31 +0200

    Version 0.1.2

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="find-similar-projects",
+    version="0.1.2",
+    author='@noisy - Krzysztof Szumny',
+    author_email='noisy.pl@gmail.com',
+    description='find-similar-projects is simple script, which grep all pip requirements files in all Github repositories.',
+    scripts=['find-similar-projects'],
+    install_requires=[
+        "lxml==3.4.4",
+        "termcolor==1.1.0",
+    ],
+)
